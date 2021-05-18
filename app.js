8a14c33832cf46c092fc95654cbc063176630ded
mirrorcall96 <mirrorcall96@gmail.com>
2021-05-18 18:58:26 +0300

    solved

--- app.js
+++ app.js
@@ -14,15 +14,40 @@
 // This functions should check the integrity of the parameters and pass true/false
 function checkParamsFn(year, month, day) {
   // Write your code here
+  if(Number.isInteger(year) && Number.isInteger(month) && Number.isInteger(day) ) return true;
+  else return false;
 }
 
 // This functions checks if the person is or above 18 years of age, return true/false
 function checkOverEighteenFn(year, month, day) {
-  // Write your code here
+  let currentTime = new Date()
+  let Thismonth = currentTime.getMonth() + 1
+  let Thisday = currentTime.getDate()
+  let Thisyear = currentTime.getFullYear()
+  if (Thisyear - year > 18) return true;
+  else if (Thisyear - year === 18){
+    if (month>Thismonth) return true;
+    else if (month===Thismonth){
+      if(day >=Thisday){
+        return true;
+      }
+      else return false 
+    }
+    else return false 
+  }
+  else return false
+  
 }
 
 function calculateAgeFn(year, month, day) {
-  // Write your code here
+  if(!checkParamsFn(year, month, day)) return "error: Bad inputs";
+  if(!checkOverEighteenFn(year, month, day)) return "error: Under 18";
+  let currentTime = new Date()
+
+  let Thismonth = currentTime.getMonth() + 1
+  let Thisday = currentTime.getDate()
+  let Thisyear = currentTime.getFullYear()
+  return Math.round(((Thisyear-year)*365 + (Thismonth-month)*30 + (Thisday-day)) / 365);
 }
 
 // Look at the naming of the functions. it looks like salwaBaqer, where
